77a65bf7d1e264d7a1fd3199b80bb8201dc06bb9
AlanSavio25 <alan.s.paul09@gmail.com>
2024-07-29 18:36:07 +0200

    add scores_unmasked and map mask again for eval

--- maploc/models/orienternet.py
+++ maploc/models/orienternet.py
@@ -210,7 +210,7 @@ class OrienterNet(BaseModel):
                     align_corners=False,
                 ).squeeze(1)
                 map_mask = ~torch.isnan(nan_mask)
-            # pred[k]["map_mask"] = map_mask if "map_mask" in data else None
+            pred[k]["map_mask"] = map_mask if "map_mask" in data else None
 
             # OrienterNet's Exhaustive Matching
 
@@ -243,7 +243,7 @@ class OrienterNet(BaseModel):
             ):
                 log_prior = pred[k]["semantic_map"]["log_prior"][0]
                 scores = scores + log_prior.unsqueeze(-1)
-            # scores_unmasked = scores.clone()
+            scores_unmasked = scores.clone()
             # pred["scores_unmasked"] = scores.clone()
             scores.masked_fill_(~map_mask[..., None], -np.inf)
             if "yaw_prior" in data:  # TODO: refactor
@@ -315,7 +315,7 @@ class OrienterNet(BaseModel):
                     "features_bev": f_bev,
                     "valid_bev": valid_bev.squeeze(1),
                     "scores": scores,
-                    # "scores_unmasked": scores_unmasked,
+                    "scores_unmasked": scores_unmasked,
                     "log_probs": log_probs,
                 }
             )
